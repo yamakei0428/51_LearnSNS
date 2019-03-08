ff9006b498426494e2e653888b7357df35021573
yamakei0428 <yamakei.aigaku.0428@icloud.com>
2019-03-08 13:33:17 +0800

    完了

--- signout.php
+++ signout.php
@@ -1,4 +1,14 @@
 <?php
+ session_start();
 
-header("Location: signin.php");
+ //セッションにユーザーのIDが保持されていたらサインインしている状態
+
+ //セッション変数の破棄
+ //ブラウザから破棄
+ $_SESSION = [];
+
+//サーバーからセッション変数のクリア
+//サーバーから破棄
+ session_destroy();
+header('Location: signin.php');
 exit();
--- timeline.php
+++ timeline.php
@@ -1,14 +1,78 @@
 <?php
 session_start();
+require 'dbconnect.php';
 
-echo '<pre>';
-var_dump($_SESSION);
-echo '</pre>';
+//サインインしていないユーザーのアクセス禁止
+if (!isset($_SESSION['51_LearnSNS']['id'])) {
+    header('Location: signin.php');
+    exit();
+}
+
+//サインインしているユーザーの情報を取得
+$sql = 'SELECT * FROM `users` WHERE `id` = ?';
+$data = [$_SESSION['51_LearnSNS']['id']];
+$stmt = $dbh->prepare($sql);
+$stmt->execute($data);
+
+$signin_user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+//echo '<pre>';
+//var_dump($signin_user);
+//echo '</pre>';
+
+//エラー内容を保持する配列
+if (!empty($_POST)) {
+    $feed = $_POST['feed'];
+
+    //空チェック
+    if ($feed != '') {
+        //投稿処理
+        //NOW（）
+        //SQLの組み込み関数
+        //現在日時を取得
+        $sql = 'INSERT INTO `feeds` (`feed`, `user_id`, `created`) VALUES (?, ?, NOW())';
+
+        //宿題1:↑この続きを書いてくる
+        //登録ができたかはphpMyAdminで確認
+
+        //宿題2:投稿処理が終わったらタイムライン画面に遷移
+        $data = [$feed, $signin_user['id']];
+        //インスタンス->メンバメソッド（引数）
+        //主体->振る舞い（利用するもの）
+        //$human->walk();
+        //$human->eat($meat);
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute($data);
+
+        //登録処理が終わったらタイムライン画面に遷移
+        //この処理を入れないとtimeline.phpにPOST送信で留まる事になる
+        //ブラウザ更新するたびにフォーム
+        header('Location: timeline.php');
+        exit();
+    } else {
+        //エラー
+        $errors['feed'] = 'blank';
+    }
+}
 
 ?>
 <?php include 'layouts/header.php'; ?>
 <body style="margin-top: 60px; background: #E4E6EB;">
-    <?php include 'navbar.php'; ?>
+<!--
+     include(ファイル名);
+     指定さてたファイルの内容をそのまま差し込む
+     同じレイアウトを複数画面から利用する時などに利用
+     ＊includeとrequireの違い
+         エラー発生時の挙動が異なる
+         include:Warningとなり、処理は続行される
+         require:Errorとなり、処理は中断
+
+          不具合が発生しても重要な問題にならないinclude
+            DB接続など重要な機能に関してreqire
+
+    読み込み元で利用可能な変数は、読み込まれたファイルの中でも使用可能
+-->
+   <?php include 'navbar.php'; ?>
     <div class="container">
         <div class="row">
             <div class="col-xs-3">
@@ -22,6 +86,9 @@ echo '</pre>';
                     <form method="POST" action="">
                         <div class="form-group">
                             <textarea name="feed" class="form-control" rows="3" placeholder="Happy Hacking!" style="font-size: 24px;"></textarea><br>
+                            <?php if (isset($errors['feed']) && $errors['feed'] == 'blank'): ?>
+                                <p class="text-danger">投稿内容を入力してください</p>
+                            <?endif; ?>
                         </div>
                         <input type="submit" value="投稿する" class="btn btn-primary">
                     </form>
